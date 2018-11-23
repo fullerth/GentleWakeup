6c3660e3cfdead4fe8d6c00869b3ac7e3fe8b4af
Thomas Fuller <thomas@mcfuller.com>
2018-11-23 08:51:37 -0800

    Add alarm time as a static offset of the initial sntp time.
    Must use RTC_DATA_ATTR to maintain values between deep sleep reboots.

--- main/sntp_example_main.c
+++ main/sntp_example_main.c
@@ -73,19 +73,21 @@ void app_main()
     }
     char strftime_buf[64];
 
-    // Set timezone to Eastern Standard Time and print local time
-    setenv("TZ", "EST5EDT,M3.2.0/2,M11.1.0", 1);
-    tzset();
-    localtime_r(&now, &timeinfo);
-    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    ESP_LOGI(TAG, "The current date/time in New York is: %s", strftime_buf);
+    RTC_DATA_ATTR static time_t alarm;
+    RTC_DATA_ATTR static uint8_t init_once = 0;
+    if(init_once == 0) {
+        alarm = now + 60;
+        init_once = 1;
+    }
+    ESP_LOGI(TAG, "Time is: %lu", now);
+    ESP_LOGI(TAG, "Alarm is: %lu", alarm);
 
-    // Set timezone to China Standard Time
-    setenv("TZ", "CST-8", 1);
+    // Set timezone to Pacific Standard Time and print local time
+    setenv("TZ", "PST8PDT", 1);
     tzset();
     localtime_r(&now, &timeinfo);
     strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    ESP_LOGI(TAG, "The current date/time in Shanghai is: %s", strftime_buf);
+    ESP_LOGI(TAG, "The current date/time on the West Coast is: %s", strftime_buf);
 
     const int deep_sleep_sec = 10;
     ESP_LOGI(TAG, "Entering deep sleep for %d seconds", deep_sleep_sec);
